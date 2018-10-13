c315cab641bf85ad490a46f99de3d459b6fb4543
ABDULHAKIM KHAMIS <you@example.com>
2018-10-13 15:02:47 +0200

    create a new project

---
+++ rest/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+var port = 3000 || process.env.PORT;
+
+app.get('/', function (req,res) {
+    res.status(200).send('Hello from new app11.')
+})
+
+app.listen(port, function(){
+    console.log('server is listening on port ' + port)
+})
