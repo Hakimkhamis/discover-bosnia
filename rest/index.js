827ade93017be62812376732fb9faf0f5f89bd7c
ABDULHAKIM KHAMIS <you@example.com>
2018-10-13 15:04:53 +0200

    Changed name

--- rest/index.js
+++ rest/index.js
@@ -4,7 +4,7 @@ var app = express();
 var port = 3000 || process.env.PORT;
 
 app.get('/', function (req,res) {
-    res.status(200).send('Hello from new app11.')
+    res.status(200).send('Hello from new app.')
 })
 
 app.listen(port, function(){
