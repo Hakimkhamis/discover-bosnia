d8c4e61f1858d014f713b233de912a19dea84096
ABDULHAKIM KHAMIS <you@example.com>
2018-10-15 09:18:48 +0200

    000

--- rest/index.js
+++ rest/index.js
@@ -4,7 +4,7 @@ var app = express();
 var port = 3000 || process.env.PORT;
 
 app.get('/', function (req,res) {
-    res.status(200).send('Hello from new app for web.')
+    res.status(200).send('Hello from new app for web1.')
 })
 
 app.listen(port, function(){
